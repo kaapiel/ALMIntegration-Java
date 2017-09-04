2720c4434cfb82fd4d27a47b5e86d5018eb3c31d
Gabriel Aguido Fraga <gabriel.fraga@yaman.com.br>
2017-09-04 14:39:14 -0300

    rest cep get

--- src/test/java/br/com/empresa/almintegration/webservices/SPRINTxx_ESTORIAxxxx_CT002.java
+++ src/test/java/br/com/empresa/almintegration/webservices/SPRINTxx_ESTORIAxxxx_CT002.java
@@ -1,7 +1,79 @@
 package br.com.empresa.almintegration.webservices;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.empresa.almintegration.alm.model.RunStep;
 import br.com.empresa.almintegration.execution.CustomerTestCase;
+import net.thucydides.core.annotations.Step;
 
 public class SPRINTxx_ESTORIAxxxx_CT002 extends CustomerTestCase {
+
+	private String endpoint;
+	private String cep = "06408200";
+	private StringBuilder RESPONSE = new StringBuilder();
+
+	@Before
+	public void beforeTest() throws Exception{
+		endpoint = "http://correiosapi.apphb.com/cep/" + cep;
+	}
+
+	@Test
+	public void testExample() throws Exception {
+		step1();
+		updateRunStepStatus(getRunIdsList().get(stepOrder++));
+		step2();
+		updateRunStepStatus(getRunIdsList().get(stepOrder++));
+	}
+
+	@Step("Realizar request no serviço dos correios (cep)")
+	public void step1(){
+		try{
+			HttpGet get = new HttpGet(endpoint);
+			HttpClient dhc = new DefaultHttpClient();
+
+			HttpResponse response = null;
+			try{
+				response = dhc.execute(get);
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			String line = "";
+
+			while ((line = rd.readLine()) != null) {
+				RESPONSE.append(line).append("\n");
+			}
+
+			currentRunStep.setField(RunStep.FIELDS.STATUS, "Passed");
+		} catch (Exception e) {
+			currentRunStep.setField(RunStep.FIELDS.STATUS, "Failed");
+		}
+	}
+
+	@Step("Receber response do serviço dos correios")
+	public void step2(){
+		try{
+
+			System.out.println(RESPONSE);
+			currentRunStep.setField(RunStep.FIELDS.STATUS, "Passed");
+		} catch (Exception e) {
+			currentRunStep.setField(RunStep.FIELDS.STATUS, "Failed");
+		}
+	}
+
+	@After
+	public void afterTest(){
+
+	}
 
 }
